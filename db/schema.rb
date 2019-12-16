4ba328447f5e5a3ce43f66f3076f1d605939da15
Alisa Siegmund <alisa.siegmund@gmail.com>
2019-12-16 21:43:33 +0100

    added models (recipe, ingredient, dose, recipe-ingredient) & created seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_15_174427) do
+ActiveRecord::Schema.define(version: 2019_12_16_201817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "doses", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.bigint "dose_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["dose_id"], name: "index_ingredients_on_dose_id"
+  end
+
+  create_table "recipe_ingredients", force: :cascade do |t|
+    t.bigint "recipe_id"
+    t.bigint "ingredient_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ingredient_id"], name: "index_recipe_ingredients_on_ingredient_id"
+    t.index ["recipe_id"], name: "index_recipe_ingredients_on_recipe_id"
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +58,7 @@ ActiveRecord::Schema.define(version: 2019_12_15_174427) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "ingredients", "doses"
+  add_foreign_key "recipe_ingredients", "ingredients"
+  add_foreign_key "recipe_ingredients", "recipes"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,64 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Creating Doses"
+
+doses = []
+d_des = ['100g', '200g', '50g', '250g']
+
+doses << Dose.create!(description: d_des[0])
+doses << Dose.create!(description: d_des[1])
+doses << Dose.create!(description: d_des[2])
+doses << Dose.create!(description: d_des[3])
+
+puts "Created 4 Doses"
+
+
+puts "Creating Ingredients"
+
+ingredients = []
+i_names = ['flour', 'butter', 'sugar', 'cinnamon', 'raisins', 'milk']
+
+ingredients << Ingredient.create!(name: i_names[0], dose: doses[0])
+ingredients << Ingredient.create!(name: i_names[1], dose: doses[1])
+ingredients << Ingredient.create!(name: i_names[2], dose: doses[2])
+ingredients << Ingredient.create!(name: i_names[3], dose: doses[3])
+ingredients << Ingredient.create!(name: i_names[4], dose: doses[2])
+ingredients << Ingredient.create!(name: i_names[5], dose: doses[1])
+
+puts "Created 6 Ingredients"
+
+puts "Creating Recipes"
+
+recipes = []
+r_names = ["Apple Cake", "Cheese Cake", "Spinach Pasta", "Lasagne"]
+r_cat = ["savory", "sweet"]
+
+recipes << Recipe.create!(name: r_names[0], category: r_cat[1])
+recipes << Recipe.create!(name: r_names[1], category: r_cat[1])
+recipes << Recipe.create!(name: r_names[2], category: r_cat[0])
+recipes << Recipe.create!(name: r_names[3], category: r_cat[0])
+
+puts "Created 4 Recipes"
+
+puts "Creating Recipe-Ingredients"
+
+recipe_ingredients = []
+
+recipe_ingredients << RecipeIngredient.create!(recipe: recipes[0], ingredient: ingredients[0])
+recipe_ingredients << RecipeIngredient.create!(recipe: recipes[0], ingredient: ingredients[1])
+recipe_ingredients << RecipeIngredient.create!(recipe: recipes[0], ingredient: ingredients[2])
+recipe_ingredients << RecipeIngredient.create!(recipe: recipes[1], ingredient: ingredients[2])
+recipe_ingredients << RecipeIngredient.create!(recipe: recipes[1], ingredient: ingredients[3])
+recipe_ingredients << RecipeIngredient.create!(recipe: recipes[1], ingredient: ingredients[5])
+recipe_ingredients << RecipeIngredient.create!(recipe: recipes[2], ingredient: ingredients[0])
+recipe_ingredients << RecipeIngredient.create!(recipe: recipes[2], ingredient: ingredients[1])
+recipe_ingredients << RecipeIngredient.create!(recipe: recipes[2], ingredient: ingredients[4])
+recipe_ingredients << RecipeIngredient.create!(recipe: recipes[3], ingredient: ingredients[0])
+recipe_ingredients << RecipeIngredient.create!(recipe: recipes[3], ingredient: ingredients[4])
+recipe_ingredients << RecipeIngredient.create!(recipe: recipes[3], ingredient: ingredients[3])
+
+puts "Adding 3 Ingredients to each Recipe"
+
+
