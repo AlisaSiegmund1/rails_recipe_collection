5806c6c87354246b3e55a062f21b458d05e46e80
Alisa Siegmund <alisa.siegmund@gmail.com>
2020-01-16 19:45:25 +0100

    initialized cloudinary, set key in env, edited new and edit view

--- db/migrate/20191216194828_create_recipes.rb
+++ db/migrate/20191216194828_create_recipes.rb
@@ -2,7 +2,7 @@ class CreateRecipes < ActiveRecord::Migration[5.2]
   def change
     create_table :recipes do |t|
       t.string :name
-      t.string :description
+      t.text :description
       t.string :category
 
       t.timestamps
