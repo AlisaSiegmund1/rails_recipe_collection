088fb81684eec8ab68d1c89f3641ba5a63be4265
Alisa Siegmund <alisa.siegmund@gmail.com>
2020-02-03 17:05:24 +0100

    added validations on name, category and photo

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,10 @@
 class Recipe < ApplicationRecord
   has_one_attached :photo
   has_many :ingredients, dependent: :destroy
+  validates :photo, presence: true
+  validates :name, presence: true
+  validates :category, presence: true
+
+
+
 end
