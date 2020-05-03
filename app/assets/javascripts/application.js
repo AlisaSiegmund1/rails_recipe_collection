86eebc43a822b574e97dcc67b152daf7fcea7346
Alisa Siegmund <alisa.siegmund@gmail.com>
2020-05-03 14:11:04 +0200

    added fav icon

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,3 +4,11 @@
 document.querySelector(".servings").addEventListener("click", function() {
   console.log("hello")
 })
+
+
+const initialAmounts = document.querySelector(".amount-ingredient").innerText;
+console.log(initialAmounts);
+
+initialAmounts.forEach((item) => {
+ console.log(item.innerText);
+});
