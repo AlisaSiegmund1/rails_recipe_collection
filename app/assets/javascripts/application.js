35d8f9b98503ec9ed560dce3756d1545f41d2ba1
Alisa Siegmund <alisa.siegmund@gmail.com>
2020-05-03 15:35:53 +0200

    added js for dynamic amounts on recipes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,14 +1,52 @@
 // = require rails-ujs
 // = require_tree .
 
-document.querySelector(".servings").addEventListener("click", function() {
-  console.log("hello")
+let initialQuantityText = document.querySelector(".servings").textContent;
+let quantity = parseInt(initialQuantityText);
+const initalQuantity = parseInt(initialQuantityText);
+
+const initialAmounts = document.querySelectorAll(".amount-ingredient");
+
+
+
+// console.log(initialAmounts);
+
+document.querySelector("#plus").addEventListener("click", function() {
+  quantity = quantity + 1;
+  document.querySelector(".servings").textContent = quantity
+
+
+})
+
+document.querySelector("#plus").addEventListener("click", function() {
+    initialAmounts.forEach((item, index) => {
+      const newItem = item.innerText;
+      let newItemInt = parseInt(newItem);
+
+      let updateItem = (newItemInt / initalQuantity) * quantity;
+      document.querySelectorAll(".amount-ingredient")[index].innerText = Math.round(updateItem).toFixed(0);
+      console.log(updateItem);
+  });
+
+})
+
+document.querySelector("#minus").addEventListener("click", function() {
+  if (quantity > 0 ) {
+    quantity = quantity - 1;
+    document.querySelector(".servings").textContent = quantity
+  }
 })
 
 
-const initialAmounts = document.querySelector(".amount-ingredient").innerText;
-console.log(initialAmounts);
 
-initialAmounts.forEach((item) => {
- console.log(item.innerText);
-});
+document.querySelector("#minus").addEventListener("click", function() {
+    initialAmounts.forEach((item, index) => {
+      const newItem = item.innerText;
+      let newItemInt = parseInt(newItem);
+
+      let updateItem = (newItemInt / initalQuantity) * quantity;
+      document.querySelectorAll(".amount-ingredient")[index].innerText = Math.round(updateItem).toFixed(0);
+      console.log(updateItem);
+  });
+
+})
