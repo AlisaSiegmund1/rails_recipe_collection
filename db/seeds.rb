08d1196c9fea16dd9bd150b8236ffff657cc6efa
Alisa Siegmund <alisa.siegmund@gmail.com>
2020-05-08 19:28:20 +0200

    Added photos in Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,42 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-# puts "Creating Doses"
-
-# doses = []
-# d_des = ['100g', '200g', '50g', '250g']
-
-# doses << Dose.create!(description: d_des[0])
-# doses << Dose.create!(description: d_des[1])
-# doses << Dose.create!(description: d_des[2])
-# doses << Dose.create!(description: d_des[3])
-
-# puts "Created 4 Doses"
-
-
-# puts "Creating Ingredients"
-
-# ingredients = []
-# i_names = ['flour', 'butter', 'sugar', 'cinnamon', 'raisins', 'milk']
-
-# ingredients << Ingredient.create!(name: i_names[0], dose: doses[0])
-# ingredients << Ingredient.create!(name: i_names[1], dose: doses[1])
-# ingredients << Ingredient.create!(name: i_names[2], dose: doses[2])
-# ingredients << Ingredient.create!(name: i_names[3], dose: doses[3])
-# ingredients << Ingredient.create!(name: i_names[4], dose: doses[2])
-# ingredients << Ingredient.create!(name: i_names[5], dose: doses[1])
-
-# puts "Created 6 Ingredients"
+require "open-uri"
 
 puts "Creating Recipes"
 
 recipes = []
-r_names = ["Apple Cake", "Cheese Cake", "Spinach Pasta", "Lasagne"]
+r_names = ["Apple Cake", "Cheese Cake", "Spaghetti Pomodoro", "Veganes Gulasch"]
 r_cat = ["savory", "sweet"]
 # r_img = cl_image_path("9hBAV7M5fcpZB8eNNv3GRnwp.png")
 r_img = "https://res.cloudinary.com/siggimsiggi/image/upload/v1580572763/9hBAV7M5fcpZB8eNNv3GRnwp.png"
@@ -47,14 +14,6 @@ recipe_two = Recipe.create!(name: r_names[1], category: r_cat[1])
 recipe_thr = Recipe.create!(name: r_names[2], category: r_cat[0])
 recipe_fou = Recipe.create!(name: r_names[3], category: r_cat[0])
 
-# puts "photo #{recipe_fou.photo.attached?}"
-
-# recipe_one.photo.attach("https://res.cloudinary.com/siggimsiggi/image/upload/v1580572763/9hBAV7M5fcpZB8eNNv3GRnwp.png")
-# recipe_two.photo.attach("https://res.cloudinary.com/siggimsiggi/image/upload/v1580572763/9hBAV7M5fcpZB8eNNv3GRnwp.png")
-# recipe_thr.photo.attach("https://res.cloudinary.com/siggimsiggi/image/upload/v1580572763/9hBAV7M5fcpZB8eNNv3GRnwp.png")
-# recipe_fou.photo.attach("https://res.cloudinary.com/siggimsiggi/image/upload/v1580572763/9hBAV7M5fcpZB8eNNv3GRnwp.png")
-
-
 recipes << recipe_one
 recipes << recipe_two
 recipes << recipe_thr
@@ -62,7 +21,20 @@ recipes << recipe_fou
 
 puts "Created 4 Recipes #{recipes}"
 
-# puts "Creating Recipe-Ingredients"
+puts "Adding Photos to Recipes"
+file_one = URI.open('https://www.eat-this.org/wp-content/uploads/2019/09/einfacher_veganer_apfelkuchen_mit_streuseln-3-800x1200@2x.jpg')
+recipe_one.photo.attach(io: file, filename: 'recipe_one.png', content_type: 'image/png')
+
+file_two = URI.open('https://images.ctfassets.net/nf38yhm0afx3/6SthzyfX2DjphyFQbW92Hb/2bb478fc3a9f883e710b630a5d6ebee2/R361_Veganer_New_York_Cheesecake21.jpg?w=1646&h=1102&fit=thumb&q=60')
+recipe_two.photo.attach(io: file, filename: 'recipe_two.png', content_type: 'image/png')
+
+file_thr = URI.open('https://www.eat-this.org/wp-content/uploads/2019/01/spaghetti-napoli-pasta-al-pomodoro-3-1280x854@2x.jpg')
+recipe_thr.photo.attach(io: file, filename: 'recipe_thr.png', content_type: 'image/png')
+
+file_fou = URI.open('https://images.ctfassets.net/nf38yhm0afx3/KVJOWFyAjZmodjo8gP0ZR/cc1aa32d8878b5479d10558fac624671/kartoffelklossegulaschrotkraut-7.jpg?w=1646&h=1102&fit=thumb&q=60')
+recipe_fou.photo.attach(io: file, filename: 'recipe_fou.png', content_type: 'image/png')
+
+puts "Added Photos to Recipes"
 
 # recipe_ingredients = []
 
