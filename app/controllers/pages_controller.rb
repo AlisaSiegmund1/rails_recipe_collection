4e14f641adc7f5893eeb7cdcba6a7a40175796a6
Alisa Siegmund <alisa.siegmund@gmail.com>
2020-05-16 14:14:36 +0200

    edited profile page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,20 @@ class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, only: [:home]
 
   def profile
+    @user = current_user
+
+    # @userRecipes =  Recipe.where("user_id == #{current_user}" )
+    #  @userRecipe.each do |recipe|
+    #     @userPublishedRecipes << recipe if recipe.publish == true
+    #   end
+
+    @userPublishedRecipes = []
+    @userUnPublishedRecipes = []
+
+
+    @user.recipe_ids.each do |recipe_id|
+    Recipe.find(recipe_id).publish == true ? @userPublishedRecipes << Recipe.find(recipe_id) : @userUnPublishedRecipes << Recipe.find(recipe_id)
+  end
+
   end
 end
